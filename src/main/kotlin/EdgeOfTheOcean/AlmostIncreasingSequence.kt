a6df94305fc91e026578225df7c6bc7380b94468
Bruno Felipe <bruno.felipe7170@gmail.com>
2020-12-19 00:00:52 -0300

    Challenge 'AlmostIncreasingSequence' completed

--- src/main/kotlin/EdgeOfTheOcean/AlmostIncreasingSequence.kt
+++ src/main/kotlin/EdgeOfTheOcean/AlmostIncreasingSequence.kt
@@ -11,5 +11,19 @@ element is also considered to be strictly increasing.
  */
 
 fun almostIncreasingSequence(sequence: MutableList<Int>): Boolean {
-    TODO()
+    var errorGreater = 0
+    var errorEqual = 0
+
+    for (index in 0 until sequence.size - 1) {
+        if (sequence[index] >= sequence[index + 1]) {
+            errorGreater++
+        }
+
+        if (index > 0) {
+            if (sequence[index - 1] >= sequence[index + 1]) {
+                errorEqual++
+            }
+        }
+    }
+    return errorGreater == 1 && errorEqual <= 1
 }
