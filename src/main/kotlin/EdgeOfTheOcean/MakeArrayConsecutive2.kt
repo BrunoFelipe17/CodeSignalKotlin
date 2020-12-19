692dbd65de9d9f2d671dbc4a24e638810f03874b
Bruno Felipe <bruno.felipe7170@gmail.com>
2020-12-18 22:02:47 -0300

    Challenge 'MakeArrayConsecutive2' completed

--- src/main/kotlin/EdgeOfTheOcean/MakeArrayConsecutive2.kt
+++ src/main/kotlin/EdgeOfTheOcean/MakeArrayConsecutive2.kt
@@ -1,5 +1,7 @@
 package EdgeOfTheOcean
 
+import kotlin.math.abs
+
 /*
 Ratiorg got statues of different sizes as a present from CodeMaster for his birthday,
 each statue having an non-negative integer size.
@@ -9,6 +11,24 @@ He may need some additional statues to be able to accomplish that.
 Help him figure out the minimum number of additional statues needed.
  */
 
+//fun makeArrayConsecutive2(statues: MutableList<Int>): Int {
+//    if (statues.size < 2) return 0
+//
+//    var counter = 0
+//
+//    statues.sort()
+//
+//    for (index in 0 until statues.size - 1) {
+//        val calc = abs(statues[index] - statues[index + 1])
+//        if (calc > 1) counter += calc - 1
+//    }
+//    return counter
+//}
+
+
+// Refactor
+
 fun makeArrayConsecutive2(statues: MutableList<Int>): Int {
-    TODO()
+    statues.sort()
+    return statues[statues.lastIndex] - statues[0] - statues.size + 1
 }
