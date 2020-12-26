4fa05f5d4738095d7c7e7107f065de583c4042d3
Bruno Felipe <bruno.felipe7170@gmail.com>
2020-12-26 19:36:51 -0300

    Challenge 'ReverseInParenthesis' completed

--- src/main/kotlin/SmoothSailing/ReverseInParenthesis.kt
+++ src/main/kotlin/SmoothSailing/ReverseInParenthesis.kt
@@ -3,5 +3,23 @@ package SmoothSailing
 // Write a function that reverses characters in (possibly nested) parentheses in the input string.
 
 fun reverseInParentheses(inputString: String): String {
-    TODO()
+    var result = inputString
+    var openParenthesisIndex = result.lastIndexOf('(')
+
+    while (openParenthesisIndex != -1) {
+        // First we need to get the last open parenthesis,
+        // then we need to find the correspondent close parenthesis using the 'indexOf' from the
+        // open parenthesis index
+
+        val closeParenthesisIndex = result.indexOf(')', openParenthesisIndex)
+
+        result = result.replace(
+            result.substring(openParenthesisIndex, closeParenthesisIndex + 1),
+            result.substring(openParenthesisIndex + 1, closeParenthesisIndex).reversed()
+        )
+
+        openParenthesisIndex = result.lastIndexOf('(')
+    }
+
+    return result
 }
