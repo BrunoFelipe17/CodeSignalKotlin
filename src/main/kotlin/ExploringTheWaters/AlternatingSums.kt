0c42a5a6f83a0dc7cf6cffbb3ee6d85e8c2605c6
Bruno Felipe <bruno.felipe7170@gmail.com>
2020-12-26 23:25:01 -0300

    Challenge 'AlternatingSums' completed

--- src/main/kotlin/ExploringTheWaters/AlternatingSums.kt
+++ src/main/kotlin/ExploringTheWaters/AlternatingSums.kt
@@ -1,5 +1,16 @@
 package ExploringTheWaters
 
 fun alternatingSums(a: MutableList<Int>): MutableList<Int> {
-    TODO()
+    val sum = arrayOf(0, 0)
+
+    a.mapIndexed { index, value ->
+        if (index % 2 == 0) {
+            sum[0] += value
+        } else {
+            sum[1] += value
+        }
+
+    }
+
+    return sum.toMutableList()
 }
