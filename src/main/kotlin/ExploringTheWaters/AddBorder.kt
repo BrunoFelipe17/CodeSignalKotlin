c4584d2492a344db4055c8c76da25d75fe3d1ec6
Bruno Felipe <bruno.felipe7170@gmail.com>
2020-12-28 20:58:02 -0300

    Challenge 'AddBorder' completed

--- src/main/kotlin/ExploringTheWaters/AddBorder.kt
+++ src/main/kotlin/ExploringTheWaters/AddBorder.kt
@@ -1,5 +1,7 @@
 package ExploringTheWaters
 
-fun addBorder(picture: MutableList<String>): MutableList<String> {
-    TODO()
-}
+fun addBorder(picture: MutableList<String>) =
+    picture.map { "*$it*" }.toMutableList().apply {
+        add(0, "*".repeat(picture[0].length + 2))
+        add("*".repeat(picture[0].length + 2))
+    }
