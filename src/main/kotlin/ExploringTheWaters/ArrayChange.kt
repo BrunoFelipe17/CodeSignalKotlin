8e3d477fc202acc25045e850655ca538c7356bd2
Bruno Felipe <bruno.felipe7170@gmail.com>
2020-12-28 22:13:03 -0300

    Challenge 'ArrayChange' completed

--- src/main/kotlin/ExploringTheWaters/ArrayChange.kt
+++ src/main/kotlin/ExploringTheWaters/ArrayChange.kt
@@ -1,5 +1,14 @@
 package ExploringTheWaters
 
 fun arrayChange(inputArray: MutableList<Int>): Int {
-    TODO()
+    var result = 0
+    for (index in 1 until inputArray.size) {
+        if (inputArray[index - 1] >= inputArray[index]) {
+            val calc = Math.abs(inputArray[index - 1] - inputArray[index]) + 1
+            inputArray[index] += calc
+            result += calc
+        }
+    }
+
+    return result
 }
