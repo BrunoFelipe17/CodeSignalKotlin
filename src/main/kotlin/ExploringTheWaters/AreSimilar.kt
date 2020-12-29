c62b36fc5a7ab30fa43e08d1bd62a766cf9d147e
Bruno Felipe <bruno.felipe7170@gmail.com>
2020-12-28 21:57:46 -0300

    Challenge 'AreSimilar' completed

--- src/main/kotlin/ExploringTheWaters/AreSimilar.kt
+++ src/main/kotlin/ExploringTheWaters/AreSimilar.kt
@@ -1,5 +1,5 @@
 package ExploringTheWaters
 
 fun areSimilar(a: MutableList<Int>, b: MutableList<Int>): Boolean {
-    TODO()
+    return a.sorted() == b.sorted() && a.zip(b).filter { it.first != it.second }.count() < 3
 }
